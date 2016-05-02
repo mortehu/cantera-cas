336f040a62aedf3fbb4df1afbe9c7b06a91fc497
Morten Hustveit <morten.hustveit@gmail.com>
2016-05-02 18:39:39 -0400

    client.cc: Fix printf format string
    A parameter was removed in a previous commit, but the format string was not
    updated.

--- src/client.cc
+++ src/client.cc
@@ -1,3 +1,19 @@
+// Copyright 2013, 2014, 2015, 2016 Morten Hustveit <morten.hustveit@gmail.com>
+// Copyright 2013, 2014, 2015, 2016 eVenture Capital Partners
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "client.h"
 
 #include <cinttypes>
@@ -379,7 +395,7 @@ kj::Promise<void> CASClient::Impl::Connect() {
 }
 
 void CASClient::Impl::HandleError(kj::Exception e) {
-  syslog(LOG_ERR, "Error connecting to \"%s\" \"%s\": %s:%d: %s", addr.c_str(),
+  syslog(LOG_ERR, "Error connecting to \"%s\": %s:%d: %s", addr.c_str(),
          e.getFile(), e.getLine(), e.getDescription().cStr());
 
   cas_client = nullptr;
