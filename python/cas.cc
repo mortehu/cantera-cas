99b6f14876c7fc872c520963c8b9ad3904ec654d
Morten Hustveit <morten.hustveit@gmail.com>
2016-05-09 18:51:56 -0400

    Support multiple simultaneous insert in Python 3 API

--- python/cas.cc
+++ python/cas.cc
@@ -15,53 +15,49 @@ struct CASClientState {
   cantera::CASClient cas_client;
 };
 
-// CASClientState singleton constructor.
-CASClientState* GetState() {
-  static CASClientState state;
-  return &state;
-}
-
 // Converts a Python 'bytes' object to cantera::string_view.  Throws an
 // exception if the input is not a 'bytes' object.
 cantera::string_view AsStringView(PyObject* value) {
   KJ_REQUIRE(PyBytes_Check(value));
-  return {PyBytes_AS_STRING(value), static_cast<size_t>(PyBytes_GET_SIZE(value))};
+  return {PyBytes_AS_STRING(value),
+          static_cast<size_t>(PyBytes_GET_SIZE(value))};
 }
 
 }  // namespace
 
 PyObject* get(PyObject* key_arg) {
   try {
-    auto state = GetState();
+    CASClientState state;
 
     if (PyList_Check(key_arg)) {
       // When a list of keys is requested, we issue simultaneous requests for
       // all the requested objects.
 
-      auto count = PyList_Size(key_arg);
+      const auto count = PyList_Size(key_arg);
 
       auto promise_array_builder =
           kj::heapArrayBuilder<kj::Promise<kj::Array<const char>>>(count);
 
       for (size_t i = 0; i < count; ++i) {
         const auto key_item = PyList_GetItem(key_arg, i);
-        promise_array_builder.add(state->cas_client.GetAsync(AsStringView(key_item)));
+        promise_array_builder.add(
+            state.cas_client.GetAsync(AsStringView(key_item)));
       }
 
       auto datas = kj::joinPromises(promise_array_builder.finish())
-                       .wait(state->aio_context.waitScope);
+                       .wait(state.aio_context.waitScope);
 
       auto result = PyList_New(count);
 
       for (size_t i = 0; i < count; ++i) {
-        auto& data = datas[i];
+        const auto& data = datas[i];
         PyList_SetItem(result, i,
                        PyBytes_FromStringAndSize(data.begin(), data.size()));
       }
 
       return result;
     } else {
-      const auto data = state->cas_client.Get(AsStringView(key_arg));
+      const auto data = state.cas_client.Get(AsStringView(key_arg));
 
       return PyBytes_FromStringAndSize(reinterpret_cast<const char*>(&data[0]),
                                        data.size());
@@ -74,9 +70,41 @@ PyObject* get(PyObject* key_arg) {
 
 PyObject* put(PyObject* data) {
   try {
-    const auto key = GetState()->cas_client.Put(PyBytes_AS_STRING(data), PyBytes_GET_SIZE(data));
+    CASClientState state;
 
-    return PyBytes_FromStringAndSize(key.data(), key.size());
+    if (PyList_Check(data)) {
+      // When a list of byte objects is provided, we issue simultaneous
+      // writes for all objects.
+
+      const auto count = PyList_Size(data);
+
+      auto promise_array_builder =
+          kj::heapArrayBuilder<kj::Promise<std::string>>(count);
+
+      for (size_t i = 0; i < count; ++i) {
+        const auto item = PyList_GetItem(data, i);
+        promise_array_builder.add(
+            state.cas_client.PutAsync(AsStringView(item)));
+      }
+
+      auto keys = kj::joinPromises(promise_array_builder.finish())
+                      .wait(state.aio_context.waitScope);
+
+      auto result = PyList_New(count);
+
+      for (size_t i = 0; i < count; ++i) {
+        const auto& key = keys[i];
+        PyList_SetItem(result, i,
+                       PyBytes_FromStringAndSize(key.data(), key.size()));
+      }
+
+      return result;
+    } else {
+      const auto key =
+          state.cas_client.Put(PyBytes_AS_STRING(data), PyBytes_GET_SIZE(data));
+
+      return PyBytes_FromStringAndSize(key.data(), key.size());
+    }
   } catch (kj::Exception e) {
     return PyErr_Format(PyExc_RuntimeError, "CAS put error: %s:%d: %s",
                         e.getFile(), e.getLine(), e.getDescription().cStr());
