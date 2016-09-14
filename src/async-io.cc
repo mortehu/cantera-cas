92904f0b7bdc6a2b1a22f98f3dc6105d1c014908
Aleksey Demakov <ademakov@gmail.com>
2016-09-14 17:17:12 +0600

    Fix build on systems with missing pipe2() syscall

--- src/async-io.cc
+++ src/async-io.cc
@@ -58,10 +58,32 @@ void SignalHandler(union sigval val) {
 
 }  // namespace
 
+#if !HAVE_PIPE2
+static void CloseOnExec(int fd) {
+  int flags;
+  KJ_SYSCALL(flags = fcntl(fd, F_GETFD));
+  if ((flags & FD_CLOEXEC) == 0) {
+    KJ_SYSCALL(fcntl(fd, F_SETFD, flags | FD_CLOEXEC));
+  }
+}
+#endif
+
 AsyncIOServer::AsyncIOServer(kj::AsyncIoContext& async_io)
     : event_promise_(nullptr) {
   int pipe[2];
+#if HAVE_PIPE2
   KJ_SYSCALL(::pipe2(pipe, O_CLOEXEC));
+#else
+  KJ_SYSCALL(::pipe(pipe));
+  try {
+    CloseOnExec(pipe[0]);
+    CloseOnExec(pipe[1]);
+  } catch (...) {
+    close(pipe[0]);
+    close(pipe[1]);
+    throw;
+  }
+#endif
   event_writer_ = kj::AutoCloseFd(pipe[1]);
   event_reader_ = async_io.lowLevelProvider->wrapInputFd(pipe[0]);
   event_promise_ =
